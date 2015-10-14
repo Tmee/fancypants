175bab8a027c3054d28f88ac9c2da91179da5b9d
keithhopkins <knhopkins412@gmail.com>
2015-10-13 20:07:08 -0600

    adds instagram call

--- application.js
+++ application.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
 });
+
+function getInstagramPics(keyword){
+  $.ajax({
+    method: 'GET',
+    url: "https://api.instagram.com/v1/tags/" + keyword + "/media/recent?client_id="+instagramKey
+  }).done(function(response){
+    var pics = JSON.parse(response.data.body).data;
+    return pics;
+  });
+}
